6bdd3c2a6413629da7f9200516ea9a4b2bfe9527
Douglas Jordan <douglas.jordan@airbnb.com>
2023-07-25 13:39:23 -0700

    monitoring: increase pilot_debounce_time buckets
    This CL increases the pilot_debounce_time histogram buckets. As our
    debounce time is often > 30s, we'd like to increase the fidelity in our
    metric in order to more accurately report on our SLO. See
    https://docs.google.com/document/d/1PvFrOp2GxwP9RRL0iXmFHtSYyqf8bs7Y56E93ncmRFg
    
    Change-Id: I996a7f2a551a913798f46178449adb5b1a13d427
    Reviewed-on: https://gerrit.musta.ch/c/public/istio/+/5526
    Reviewed-by: Edie Yang <edie.yang@airbnb.com>
    Reviewed-on: https://gerrit.musta.ch/c/public/istio/+/6013
    Reviewed-by: Douglas Jordan <douglas.jordan@airbnb.com>

--- pilot/pkg/xds/monitoring.go
+++ pilot/pkg/xds/monitoring.go
@@ -98,7 +98,7 @@ var (
 	debounceTime = monitoring.NewDistribution(
 		"pilot_debounce_time",
 		"Delay in seconds between the first config enters debouncing and the merged push request is pushed into the push queue.",
-		[]float64{.01, .1, 1, 3, 5, 10, 20, 30},
+		[]float64{1, 5, 10, 20, 30, 40, 50, 60},
 	)
 
 	pushContextInitTime = monitoring.NewDistribution(
